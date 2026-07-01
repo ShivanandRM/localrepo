822ed75a7048283a4f7b27b5e338eee762fe4681
Shivanand <shivanandmugali882@gmail.com>
2026-07-01 12:06:39 +0530

    Add button

--- name.java
+++ name.java
@@ -2,7 +2,7 @@ package LocalRepo;
 
 public class name {
     public static void main(String[] args) {
-        System.out.println("Shivanand");
+        System.out.println("Shivanand mugali");
         System.out.println("Good Morning");
     }
 }
