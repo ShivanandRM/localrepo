9a000c26028c1c916c2f68890ce27c169a58c8a5
Shivanand <shivanandmugali882@gmail.com>
2026-07-21 18:00:57 +0530

    bubblesort example

---
+++ bubble1.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Arrays;
+
+public class bubble1 {
+    public static void main(String[] args) {
+        int[] arr = {1,5,4,2,3};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void bubble(int[] arr){
+        boolean swap;
+        for(int i =0; i<arr.length;i++){
+            swap = false;
+            for(int j = 1; j<arr.length-i;j++){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap){
+                // br/eak;
+            }
+        }
+    }
+}
